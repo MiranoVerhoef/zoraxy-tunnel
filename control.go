66fb7ffa54c5596dc3bb37b0a0df38627d4ab29c
Mirano <67699857+MiranoVerhoef@users.noreply.github.com>
2026-09-14 13:49:36 +0200

    Record connector update mode

--- control.go
+++ control.go
@@ -70,6 +70,7 @@ func (c *controlServer) handle(conn net.Conn) {
 		sess.Close()
 		return
 	}
+	recordConnectorMode(tunnelID, connectorID, req.UpdateMode)
 	now := time.Now().UTC()
 	s := &session{yamux: sess, connectorID: connectorID, joined: now, lastActivity: now, remoteAddr: remote, clientVersion: req.Version, clientHostname: req.Hostname, clientOS: req.OS, clientArch: req.Arch}
 	if c.registry.register(tunnelID, connectorID, s) {
